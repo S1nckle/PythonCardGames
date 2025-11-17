4c96a0c7f1c8a210c8ce9d39af5ae6f11f06b464
Пользователь <LeoSerg2007@gmail.com>
2025-11-17 15:22:14 +0500

    BlackJack initial commit

---
+++ BlackJack.py
@@ -0,0 +1,160 @@
+import CardPile
+
+class Dealer(CardPile.Hand):
+    def __init__(self):
+        super().__init__()
+
+    def show_drawn(self):
+        """
+        Show cards when given with one closed
+        :return:
+        """
+        return f"{self.get_hand()[0]}, ?"
+
+class BlackJack:
+    HIT = 'H'
+    SPLIT = 'S'
+    DOUBLE = 'D'
+
+    def __init__(self):
+        self.deck = CardPile.CardPile()
+        self.player = CardPile.Hand()
+        self.dealer = Dealer()
+
+    @staticmethod
+    def count_total(hand: CardPile.Hand):
+        """
+        :param hand: Hand to count
+        :return: Total score of given hand via rules of Black Jack
+        """
+        total = 0
+        aces = 0
+
+        for card in hand.get_hand():
+            if card.get_rank() > 1:
+                total += min(10, card.get_rank())
+            else:
+                aces += 1
+        if aces > 0:
+            total += aces - 1
+            total += 11 if total <= 10 else 1
+
+        return total
+
+    def print_drawn_state(self):
+        """
+        :return: Pseudographic UI of the game
+        """
+        print('|====================Black Jack====================|')
+        print("Dealer: ", self.dealer.show_drawn(), end='\n\n')
+        print("Player: ", self.player, end='\n')
+        print('|============(Dealer must take until 17)===========|', end='\n\n')
+
+    def print_state(self):
+        """
+        :return: Pseudographic UI of the game
+        """
+        print('|====================Black Jack====================|')
+        print("Dealer: ", self.dealer, end='\n\n')
+        print("Player: ", self.player, end='\n')
+        print('|============(Dealer must take until 17)===========|', end='\n\n')
+
+    def play(self, bet=0):
+        doubled = False
+        for i in range(2):
+            self.dealer.take(self.deck.draw())
+            self.player.take(self.deck.draw())
+        self.print_drawn_state()
+
+        drawn_player = BlackJack.count_total(self.player)
+        drawn_dealer = BlackJack.count_total(self.dealer)
+
+        if drawn_player == 21:
+            self.print_state()
+            if drawn_dealer == 21:
+                print("Stay!".center(52, ' '))
+                return
+            else:
+                print("You won!".center(52, ' '))
+                return
+        elif drawn_dealer == 21:
+            self.print_state()
+            print("Dealer won!".center(52, ' '))
+            return
+
+
+        move = input('Type H to HIT, D to DOUBLE, S to SPLIT and ANY to STAY: \n')
+
+        while move == BlackJack.HIT or move == BlackJack.DOUBLE or move == BlackJack.SPLIT:
+            if move == BlackJack.HIT:
+                self.player.take(self.deck.draw())
+                self.print_drawn_state()
+                if BlackJack.count_total(self.player) <= 21:
+                    move = input('Type H to HIT, D to DOUBLE, S to SPLIT and ANY to STAY: \n')
+                else:
+                    self.print_state()
+                    print("Dealer won!".center(52, ' '))
+                    return
+            if move == BlackJack.DOUBLE:
+                self.player.take(self.deck.draw())
+                doubled = True
+                break
+            if move == BlackJack.SPLIT:
+                print("We don't split.\n")
+                move = input('Type H to HIT, D to DOUBLE, S to SPLIT and ANY to STAY: \n')
+
+
+        while BlackJack.count_total(self.dealer) < 17:
+            self.dealer.take(self.deck.draw())
+        self.print_state()
+
+        dealer_total = BlackJack.count_total(self.dealer)
+        if dealer_total > 21:
+            print("You won!".center(52, ' '))
+            return
+
+        player_total = BlackJack.count_total(self.player)
+        if player_total > dealer_total:
+            print("You won!".center(52, ' '))
+        elif player_total < dealer_total:
+            print("Dealer won!".center(52, ' '))
+        else:
+            print("Stay!".center(52, ' '))
+        return
+
+
+
+def main(money: int):
+    """
+    :param money: Start money
+    """
+    from random import randint
+    bj.deck.shuffle()
+    if not RESHUFFLE_EVERY_ROUND:
+        cut = randint(bj.deck.get_size() // 4, bj.deck.get_size() // 2)
+
+    while True:
+        bet = 0
+        if BETS:
+            bet = int(input("Place your bet: "))
+        if not RESHUFFLE_EVERY_ROUND:
+            if bj.deck.get_size() < cut:
+                bj.deck.reset()
+                bj.deck.shuffle()
+                cut = randint(bj.deck.get_size() // 4, bj.deck.get_size() // 2)
+        else:
+            bj.deck.reset()
+            bj.deck.shuffle()
+
+        bj.play(bet)
+        bj.player.clear()
+        bj.dealer.clear()
+        if input("L to Leave, ANY to continue\n\n") == 'L':
+            print("Thanks for game.")
+            break
+
+
+bj = BlackJack()
+BETS = False
+RESHUFFLE_EVERY_ROUND = False
+main(1000)
---
+++ CardPile.py
@@ -0,0 +1,134 @@
+class Card:
+    SUITS = [
+        "Hearts",
+        "Spades",
+        "Diamonds",
+        "Clubs"
+    ]
+
+    RANKS = {
+        1: "Ace",
+        2: "Two",
+        3: "Three",
+        4: "Four",
+        5: "Five",
+        6: "Six",
+        7: "Seven",
+        8: "Eight",
+        9: "Nine",
+        10: "Ten",
+        11: "Jack",
+        12: "Queen",
+        13: "King"
+    }
+
+    SUITS_COUNT = 4
+    RANKS_COUNT = 13
+
+    def __init__(self, rank: int, suit: int):
+        self.__suit__ = suit
+        self.__rank__ = rank
+
+    def get_suit(self) -> int:
+        return self.__suit__
+
+    def get_rank(self) -> int:
+        return self.__rank__
+
+    def __str__(self):
+        return f"{self.RANKS[self.get_rank()]} of {self.SUITS[self.get_suit()]}"
+
+    def __repr__(self):
+        return self.__str__()
+
+
+
+class CardPile:
+    def __init__(self):
+        self.__pile__ = []
+        for rank in range(1, Card.RANKS_COUNT):
+            for suit in range(Card.SUITS_COUNT):
+                self.__pile__.append(Card(rank, suit))
+        self.__size__ = len(self.__pile__)
+
+    def is_not_empty(self) -> bool:
+        return self.__size__ > 0
+
+    def get_pile(self) -> list:
+        """
+        :return: List of cards in pile
+        """
+        return self.__pile__.copy()
+
+    def get_size(self) -> int:
+        """
+        :return: Current quantity of cards in pile.
+        """
+        return self.__size__
+
+    def shuffle(self):
+        """
+        Shuffles current pile
+        :return: None
+        """
+        from random import randint
+        new_pile = []
+        while len(self.__pile__) > 0:
+            new_pile.append(self.__pile__.pop(randint(0, len(self.__pile__) - 1)))
+        self.__pile__ = new_pile
+        print("Riffle shuffle!")
+
+    def draw(self) -> Card:
+        """
+        Takes out the top card from pile. Used in pair with Hand.take()
+        :return: The taken card
+        """
+        if self.__size__ < 1:
+            raise ValueError("Could not draw from clear pile!")
+        self.__size__ -= 1
+        return self.__pile__.pop(0)
+
+    def reset(self):
+        """
+        Resets current pile to default. Needs to be reshuffled after.
+        :return:
+        """
+        self.__pile__ = []
+        for rank in range(1, Card.RANKS_COUNT):
+            for suit in range(Card.SUITS_COUNT):
+                self.__pile__.append(Card(rank, suit))
+        self.__size__ = len(self.__pile__)
+        print("Deck reset!")
+
+    def __str__(self):
+        return ', '.join([str(card) for card in self.__pile__])
+
+
+class Hand:
+    def __init__(self):
+        self.__hand__ = []
+
+    def get_hand(self) -> list:
+        """
+        :return: List of cards in hand
+        """
+        return self.__hand__.copy()
+
+    def take(self, card: Card) -> Card:
+        """
+        Adds given card to hand
+        :param card: Given card
+        :return: The same card
+        """
+        self.__hand__.append(card)
+        return card
+
+    def clear(self):
+        """
+        Removes all cards from hand
+        :return:
+        """
+        self.__hand__.clear()
+
+    def __str__(self):
+        return ', '.join([str(card) for card in self.__hand__])
